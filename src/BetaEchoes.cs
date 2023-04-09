83c76125773e68c6325e5a8ecba99915feb4ddd2
SortaUnknown <roi.anubis@gmail.com>
2023-04-09 19:22:11 +0200

    Do not explode when testing

--- src/BetaEchoes.cs
+++ src/BetaEchoes.cs
@@ -1,4 +1,9 @@
 ﻿using BepInEx;
+using System.Security.Permissions;
+
+#pragma warning disable CS0618 //Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 //Type or member is obsolete
 
 namespace BetaEchoes
 {
